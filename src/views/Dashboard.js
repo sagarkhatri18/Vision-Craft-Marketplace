0d75950a983cf9d9dfb4b9cdd4ebfcd95526b95d
Sagar Khatri <sagarkhatri986@gmail.com>
2024-01-30 19:51:57 -0500

    templated added to git

---
+++ src/views/Dashboard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+  Form,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
+
+function Dashboard() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          {/* <Col lg="3" sm="6">
+            <Card className="card-stats">
+              <Card.Body>
+                <Row>
+                  <Col xs="5">
+                    <div className="icon-big text-center icon-warning">
+                      <i className="nc-icon nc-favourite-28 text-primary"></i>
+                    </div>
+                  </Col>
+                  <Col xs="7">
+                    <div className="numbers">
+                      <p className="card-category">Followers</p>
+                      <Card.Title as="h4">+45K</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer>
+                <hr></hr>
+                <div className="stats">
+                  <i className="fas fa-redo mr-1"></i>
+                  Update now
+                </div>
+              </Card.Footer>
+            </Card>
+          </Col> */}
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Dashboard;
